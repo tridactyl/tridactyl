1d986d0f7f9e897e5678fecc80e6d8fd22208c56
Colin Caine <cmc@cs.man.ac.uk>
2017-02-12 23:10:09 +0000

    Remove javascript sources.

--- addon/main.js
+++
@@ -1,9 +0,0 @@
-// Background script experiments.
-
-function handleBrowserAction() {
-  browser.tabs.query({active: true}).then(console.log);
-}
-
-browser.browserAction.onClicked.addListener(handleBrowserAction);
-
-console.log("Loaded Tridactyl");
