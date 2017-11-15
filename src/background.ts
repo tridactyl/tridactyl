2fd88e4b3037aea4c1b34da690fdc91991975aeb
Colin Caine <cmc@cs.man.ac.uk>
2017-11-15 00:52:11 +0000

    debug: add `tri` object to window in bg.

--- src/background.ts
+++ src/background.ts
@@ -11,3 +11,31 @@ keydown_background.onKeydown.addListener(Controller.acceptKey)
 
 // Send commandline to controller
 CommandLine.onLine.addListener(Controller.acceptExCmd)
+
+// Add various useful modules to the window for debugging
+import * as messaging from './messaging'
+import * as excmds from './excmds_background'
+import * as commandline_background from './commandline_background'
+import * as controller from './controller'
+import * as convert from './convert'
+import * as dom from './dom'
+import * as hinting_background from './hinting_background'
+import * as itertools from './itertools'
+import * as keyseq from './keyseq'
+import * as msgsafe from './msgsafe'
+import * as state from './state'
+
+(window as any).tri = Object.assign(Object.create(null), {
+    messaging,
+    excmds,
+    commandline_background,
+    controller,
+    convert,
+    dom,
+    hinting_background,
+    itertools,
+    keydown_background,
+    keyseq,
+    msgsafe,
+    state,
+})
