4a271805bb1f87ab84106d9c42bd089dcb893a40
Colin Caine <cmc@cs.man.ac.uk>
2017-11-28 17:36:40 +0000

    build: BSD compatibility for git_version.sh

--- scripts/git_version.sh
+++ scripts/git_version.sh
@@ -4,4 +4,5 @@ gitversion=$(git describe --tags | cut -d"-" -f2-)
 manversion=$(grep '"version":' ./src/manifest.json | cut -d":" -f2 | tr -d \" | tr -d , | cut -d" " -f2)
 version=$manversion-$gitversion
 
-sed -i 's/REPLACE_ME_WITH_THE_VERSION_USING_SED/'$version'/' ./build/background.js
+sed -i.bak 's/REPLACE_ME_WITH_THE_VERSION_USING_SED/'$version'/' ./build/background.js
+rm ./build/background.js.bak
