8d5c6d8be021abc12186bf21d467660506b53014
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2017-12-26 11:21:31 +0000

    non working order

--- src/requests.ts
+++ src/requests.ts
@@ -3,19 +3,32 @@ export function addurltocsp(response){
     let cspind = headers.findIndex(header => header.name == "Content-Security-Policy")
     // if it's found
     if (cspind > -1) {
-        // sledgehammer approach: delete it
+        
+        // Split the csp header up so we can manage it individually.
+        let csparr = headers[cspind]["value"].split("; ")
+        console.log(headers[cspind]["value"])
+        
+        // Turbo agressive, definitely not secure
+        let i = 0;
+        for (i = 0; i < csparr.length; i++) {
+            if (csparr[i] == "sandbox") {
+                csparr.splice(i,1)
+            }
+            if (csparr[i].indexOf("style-src") != -1) {
+                if(csparr[i].indexOf("'self'") != -1) {
+                    csparr[i].replace("'self'","'unsafe-inline'")
+                } else {
+                    csparr[i].concat(" 'unsafe-inline'")
+                }
+            } else if (csparr[i].indexOf("style-src") > -1) {
+                csparr.splice(i,1, "style-src 'unsafe-inline'")
+            }
+        }
 
-        headers[cspind]["value"] = "default-src 'self' moz-extension://1da95d91-ca7f-4cda-abee-36e926a5e2a1; style-src 'unsafe-inline'"
-        //
-        // Got this thing working but I want to see what I can enable without compromising csp.
-        //headers[cspind]["value"] = headers[cspind]["value"].replace(
-        //     "script-src ","script-src moz: ").replace(
-        //         "default-src ","default-src moz: ").replace(
-        //             "sandbox","sandbox allow-forms allow-same-origin allow-scripts allow-popups allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-popups-to-escape-sandbox allow-top-navigation").split("none").join("self")
+        console.log(csparr)
+        headers[cspind]["value"] = csparr.join("; ")
+        console.log(headers[cspind]["value"])
 
-        //
-        //     Hard coded doesn't work. Missing something.
-        // headers[cspind]["value"] = "style-src 'self'; report-uri https://sentry.mafiasi.de/api/15/csp-report/?sentry_key=30c7c96fda184c3981f2ee5aa7cf8fa9; default-src moz: 'self'; script-src moz: 'self' sentry.mafiasi.de 'nonce-GQTbfotsa2WChjMo' 'nonce-SnTfBGzkDF5ABVyG' 'nonce-04f00A0OQnOBQv5Q'; img-src 'self' data:"
     }
     // min version FF57
     // headers is an array of objects with name, value
