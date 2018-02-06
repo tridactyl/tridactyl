3ac4441208badfd1d111185c321a5cadf5eaa2d6
Isaac Khor <s.github@isaackhor.com>
2018-02-06 23:16:26 +0800

    Add bootstrap

--- src/settings_page.ts
+++ src/settings_page.ts
@@ -1,3 +1,6 @@
 // Script used by the settings page.
 
 import * as parser from './parsers/exmode'
+
+const textSettings = document.querySelector('#userSettings') as HTMLTextAreaElement
+const currentSettings = document.querySelector('#currentSettings') as HTMLDivElement
