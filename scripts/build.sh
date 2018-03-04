119758454f2eae7bd459f025d9fb2bf67ca0ab1d
glacambre <code@lacamb.re>
2018-03-04 14:15:26 +0100

    Add cleanslate.css

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,16 @@ set -e
 
 PATH=$(npm bin):"$PATH"
 export PATH
+CLEANSLATE="node_modules/cleanslate/docs/files/cleanslate.css"
 
 mkdir -p generated/static
 scripts/excmds_macros.py
 scripts/newtab.md.sh
+if [ -e "$CLEANSLATE" ] ; then
+	cp "$CLEANSLATE" build/static/cleanslate.css
+else
+	echo "Couldn't find cleanslate.css. Try running 'npm install'"
+fi
 scripts/make_docs.sh &
 
 (webpack --display errors-only && scripts/git_version.sh)&
