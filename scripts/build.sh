7750609e6aff93dea5b9fc954d9c80845ebeb679
glacambre <code@lacamb.re>
2018-03-04 14:42:35 +0100

    build.sh: Fix script copying cleanslate.css to potentially non-existing dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,13 +9,14 @@ CLEANSLATE="node_modules/cleanslate/docs/files/cleanslate.css"
 mkdir -p generated/static
 scripts/excmds_macros.py
 scripts/newtab.md.sh
-if [ -e "$CLEANSLATE" ] ; then
-	cp "$CLEANSLATE" build/static/cleanslate.css
-else
-	echo "Couldn't find cleanslate.css. Try running 'npm install'"
-fi
 scripts/make_docs.sh &
 
 (webpack --display errors-only && scripts/git_version.sh)&
 
 wait
+
+if [ -e "$CLEANSLATE" ] ; then
+	cp "$CLEANSLATE" build/static/cleanslate.css
+else
+	echo "Couldn't find cleanslate.css. Try running 'npm install'"
+fi
