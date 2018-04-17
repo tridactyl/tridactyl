1ef49cb6e841f721b332bcdaf0c34f79599d0a64
Colin Caine <cmc@cs.man.ac.uk>
2018-04-17 01:09:24 +0100

    Fix :unbind
    Unbinding actually sets the property to `""`, but the keyseq code
    rightly doesn't know about that convention.
    
    For now I'm putting some special casing in the normalmode parser.
    
    Fixes #398.

--- src/parsers/normalmode.ts
+++ src/parsers/normalmode.ts
@@ -4,5 +4,13 @@ import * as config from "../config"
 import * as keyseq from "../keyseq"
 
 export function parser(keys): keyseq.ParserResponse {
-    return keyseq.parse(keys, keyseq.mapstrObjToKeyMap(config.get("nmaps")))
+    const result = keyseq.parse(
+        keys,
+        keyseq.mapstrObjToKeyMap(config.get("nmaps")),
+    )
+    if (result.value === "") {
+        // This is supposed to be unbound, so pretend it didn't match
+        return { keys: [], isMatch: false }
+    }
+    return result
 }
