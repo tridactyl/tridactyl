2626d292f07ee8e9b0ffc93089fa0ad768423127
Colin Caine <cmc@cs.man.ac.uk>
2018-04-18 11:07:57 +0100

    Fix :unbind better
    Remove all unbound keys before passing to keyseq.parse(). Resolves #402.

--- src/parsers/normalmode.ts
+++ src/parsers/normalmode.ts
@@ -4,13 +4,11 @@ import * as config from "../config"
 import * as keyseq from "../keyseq"
 
 export function parser(keys): keyseq.ParserResponse {
-    const result = keyseq.parse(
-        keys,
-        keyseq.mapstrObjToKeyMap(config.get("nmaps")),
-    )
-    if (result.value === "") {
-        // This is supposed to be unbound, so pretend it didn't match
-        return { keys: [], isMatch: false }
-    }
-    return result
+    let nmaps: any = config.get("nmaps")
+    // Remove unbound keys
+    nmaps = Object.entries(nmaps).filter(([k, v]) => v !== "")
+    // Convert to KeyMap
+    nmaps = keyseq.mapstrMapToKeyMap(new Map(nmaps))
+
+    return keyseq.parse(keys, nmaps)
 }
