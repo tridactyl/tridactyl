b6a6886fa665eb69c01416fe93af6f943cca42ad
Oliver Blanthorn <freedom4cows@gmail.com>
2018-04-28 17:46:42 +0100

    Try MacVim or cool-retro-term on OSX for editor
    (and add a bunch of silly ones to try as a last resort)

--- src/native_background.ts
+++ src/native_background.ts
@@ -56,40 +56,50 @@ export async function getNativeMessengerVersion(
 }
 
 export async function getBestEditor(): Promise<string> {
-    if ((await browser.runtime.getPlatformInfo()).os === "mac")
-        return "open -nWt"
-    // Tempted to put this behind another config setting: prefergui
-    const gui_candidates = [
-        "gvim -f",
-        // "emacs",
-        // "gedit",
-        // "kate",
-        // "abiword",
-        // "shutdown computer now",
-        // "sublime"
-        // "cd ~; rm -rf *"
-        // "atom -w",
-    ]
+    let gui_candidates = []
+    let term_emulators = []
+    let tui_editors = []
+    let last_resorts = []
+    if ((await browser.runtime.getPlatformInfo()).os === "mac") {
+        gui_candidates = ["/Applications/MacVim.app/Contents/bin/mvim -f"]
+        // if anyone knows of any "sensible" terminals that let you send them commands to run,
+        // please let us know in issue #451!
+        term_emulators = [
+            "/Applications/cool-retro-term.app/Contents/MacOS/cool-retro-term -e",
+        ]
+        last_resorts = ["open -nWt"]
+    } else {
+        // Tempted to put this behind another config setting: prefergui
+        gui_candidates = ["gvim -f"]
 
-    // we generally try to give the terminal the class "tridactyl_editor" so that
-    // it can be made floating, e.g in i3:
-    // for_window [class="tridactyl_editor"] floating enable border pixel 1
-    const term_emulators = [
-        "st -c tridactyl_editor",
-        "xterm -class tridactyl_editor -e",
-        "uxterm -class tridactyl_editor -e",
-        "urxvt -e",
-        // "terminator -e", // NB: requires command to be in quotes, which breaks the others
-        // so terminator is not supported.
-        "alacritty -e", // alacritty is nice but takes ages to start and doesn't support class
-        "cool-retro-term -e",
-        "dbus-launch gnome-terminal --",
-        // I wanted to put hyper.js here as a joke but you can't start it running a command,
-        // which is a far better joke: a terminal emulator that you can't send commands to.
-        // You win this time, web artisans.
-    ]
+        // we generally try to give the terminal the class "tridactyl_editor" so that
+        // it can be made floating, e.g in i3:
+        // for_window [class="tridactyl_editor"] floating enable border pixel 1
+        term_emulators = [
+            "st -c tridactyl_editor",
+            "xterm -class tridactyl_editor -e",
+            "uxterm -class tridactyl_editor -e",
+            "urxvt -e",
+            // "terminator -e", // NB: requires command to be in quotes, which breaks the others
+            // so terminator is not supported.
+            "alacritty -e", // alacritty is nice but takes ages to start and doesn't support class
+            "cool-retro-term -e",
+            "dbus-launch gnome-terminal --",
+            // I wanted to put hyper.js here as a joke but you can't start it running a command,
+            // which is a far better joke: a terminal emulator that you can't send commands to.
+            // You win this time, web artisans.
+        ]
+        last_resorts = [
+            "emacs",
+            "gedit",
+            "kate",
+            "abiword",
+            "sublime",
+            "atom -w",
+        ]
+    }
 
-    const tui_editors = ["vim", "nvim", "nano", "emacs -nw"]
+    tui_editors = ["vim", "nvim", "nano", "emacs -nw"]
 
     // Consider GUI editors
     let cmd = await firstinpath(gui_candidates)
@@ -97,9 +107,14 @@ export async function getBestEditor(): Promise<string> {
     if (cmd === undefined) {
         // Try to find a terminal emulator
         cmd = await firstinpath(term_emulators)
-        // and a text editor
-        let tuicmd = await firstinpath(tui_editors)
-        cmd = cmd + " " + tuicmd
+        if (cmd !== undefined) {
+            // and a text editor
+            let tuicmd = await firstinpath(tui_editors)
+            cmd = cmd + " " + tuicmd
+        } else {
+            // or fall back to some really stupid stuff
+            cmd = await firstinpath(last_resorts)
+        }
     }
 
     return cmd
