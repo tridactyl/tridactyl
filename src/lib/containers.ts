ca563720df86aab45a944d5ae0995387c7e6848e
Anton <anton.v.asgeirsson@gmail.com>
2018-05-08 17:33:55 +0000

    Started work on implementing contextual identity api for tridactyl.

---
+++ src/lib/containers.ts
@@ -0,0 +1,5 @@
+export async function containerCreate({}) {}
+export async function containerRemove(containerId: string) {}
+export async function containerUpdate({}) {}
+export async function containerGet({}) {}
+export async function containerQuery({}) {}
