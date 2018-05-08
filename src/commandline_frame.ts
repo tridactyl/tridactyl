bb2d7032ba79381299929f45dfa563d8fb9aa4b9
Bruno Alphonsus de Oliveira Nascimento <caputchinefrobles@yahoo.com.br>
2018-05-08 00:20:17 -0300

    putting default theme on the new structure

--- src/commandline_frame.ts
+++ src/commandline_frame.ts
@@ -58,6 +58,32 @@ function enableCompletions() {
 }
 /* document.addEventListener("DOMContentLoaded", enableCompletions) */
 
+//https://stackoverflow.com/questions/11833759/add-stylesheet-to-head-using-javascript-in-body
+function toggleStylesheet(href, onoff) {
+    var existingNode: Node //get existing stylesheet node if it already exists:
+    for (var i = 0; i < document.styleSheets.length; i++) {
+        if (
+            document.styleSheets[i].href &&
+            document.styleSheets[i].href.indexOf(href) > -1
+        )
+            existingNode = document.styleSheets[i].ownerNode
+    }
+    if (onoff == undefined) onoff = !existingNode //toggle on or off if undefined
+    if (onoff) {
+        //TURN ON:
+        if (existingNode) return onoff //already exists so cancel now
+        var link = document.createElement("link")
+        link.rel = "stylesheet"
+        link.type = "text/css"
+        link.href = href
+        document.getElementsByTagName("head")[0].appendChild(link)
+    } else {
+        //TURN OFF:
+        if (existingNode) existingNode.parentNode.removeChild(existingNode)
+    }
+    return onoff
+}
+
 let noblur = e => setTimeout(() => clInput.focus(), 0)
 let lastTheme: string
 
@@ -67,13 +93,7 @@ export function focus() {
 
     // update theme of command line
     let theme = Config.get("theme")
-    if (theme !== lastTheme) {
-        if (lastTheme) {
-            document.querySelector(":root").classList.remove(lastTheme)
-        }
-        document.querySelector(":root").classList.add(theme)
-        lastTheme = theme
-    }
+    toggleStylesheet("themes/" + theme + "/commandline.css", 1)
 
     clInput.focus()
     clInput.addEventListener("blur", noblur)
