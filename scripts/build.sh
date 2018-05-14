19e3363c92c051dc905a13d6b29552417b220915
Babil Golam Sarwar <gsbabil@gmail.com>
2018-05-14 14:19:20 +1000

    Add MinGW build support and minor refactoring

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,25 +2,52 @@
 
 set -e
 
-PATH=$(npm bin):"$PATH"
-export PATH
 CLEANSLATE="node_modules/cleanslate/docs/files/cleanslate.css"
 
+isWindowsMingw() {
+  local is_mingw="False"
+  if [ "$(uname | cut -c 1-5)" == "MINGW" ]; then
+    is_mingw="True"
+  fi
+
+  echo -n "${is_mingw}"
+}
+
+if [ "$(isWindowsMingw)" == "True" ]; then
+  NPM_BIN_DIR="$(cygpath $(npm bin))"
+  PATH=$NPM_BIN_DIR:$PATH
+else
+  PATH="$(npm bin):$PATH"
+fi
+
+export PATH
+
+mkdir -p build
+mkdir -p build/static
 mkdir -p generated/static
 mkdir -p generated/static/clippy
+
 scripts/excmds_macros.py
 scripts/newtab.md.sh
 scripts/make_tutorial.sh
 scripts/make_docs.sh &
-nearleyc src/grammars/bracketexpr.ne > src/grammars/.bracketexpr.generated.ts
-native/install.sh local
 
-(webpack --display errors-only && scripts/git_version.sh)&
+nearleyc src/grammars/bracketexpr.ne \
+  > src/grammars/.bracketexpr.generated.ts
+
+if [ "$(isWindowsMingw)" == "True" ]; then
+  powershell native/win_install.ps1
+else
+  native/install.sh local
+fi
+
+(webpack --display errors-only \
+  && scripts/git_version.sh) &
 
 wait
 
 if [ -e "$CLEANSLATE" ] ; then
-	cp "$CLEANSLATE" build/static/cleanslate.css
+	cp -v "$CLEANSLATE" build/static/cleanslate.css
 else
 	echo "Couldn't find cleanslate.css. Try running 'npm install'"
 fi
