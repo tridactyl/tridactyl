3e2bf86291e2416e435677fd5e8f3262525d96bb
Anton <anton.v.asgeirsson@gmail.com>
2018-05-21 09:19:28 +0000

    refactored containers

--- src/lib/containers.ts
+++ src/lib/containers.ts
@@ -1,8 +1,6 @@
-import * as Logging from '../logging'
+import * as Logging from "../logging"
 const logger = new Logging.Logger("excmd")
 //TODO: refactor the whole "firefox-container" + containerId thing into something more sensible.
-
-
 
 /** Creates a container from the specified parameters. Unlike the official Mozilla addon, does not
  *  allow multiple  containers with the same (name, color, icon) constraint.
@@ -15,58 +13,61 @@ export async function containerCreate(
     color: string,
     icon: string,
 ) {
-    let c = containerFromString(name, color, icon);
+    let c = containerFromString(name, color, icon)
     let exists = await containerExists(c)
     if (!exists) {
         browser.contextualIdentities
             .create(c)
             .then(newContainer =>
-                logger.debug(`Created container: ${newContainer.cookieStoreId}`),
+                logger.info("Created container:", newContainer.cookieStoreId),
             )
     } else {
-        logger.warning('This container exists already')
+        logger.error("containerCreate: Container already exists, aborting.")
         logger.debug(c)
     }
 }
 
 /** Removes specified container.
- *  @param containerId  Expects a single digit string.
+ *  @param containerId Expects a cookieStringId e.g. "firefox-container-n".
  */
 export async function containerRemove(containerId: string) {
     browser.contextualIdentities
-        .remove("firefox-container" + containerId)
+        .remove(containerId)
         .then(removedContainer =>
-            logger.debug(`Removed container: ${removedContainer.cookieStoreId}`),
+            logger.info("Removed container:", removedContainer.cookieStoreId),
         )
 }
 
 /** Updates the specified container
- *  @param containerId  Expects a single digit string.
- *  @param containerUpdate  An object containing all parameters to update.
+ *  @param containerId Expects a cookieStringId e.g. "firefox-container-n".
+ *  @param c An object containing all parameters to update.
+ *  TODO: pass an object to this when tridactyl gets proper flag parsing
  */
-export async function containerUpdate(containerId: string, c: {} ) {
-    currcontainer = await browser.contextualIdentities.get(containerId)
-
-    if (!containerExists(c)) {
-    browser.contextualIdentities.update(containerId, 
+export async function containerUpdate(
+    containerId: string,
+    name: string,
+    color: string,
+    icon: string,
+) {
+    let currcontainer = await containerGetFromId(containerId)
+    let container = containerFromString(name, color, icon)
+    if (!containerExists(container)) {
+        browser.contextualIdentities.update(containerId, container)
     } else {
-        logger.debug("Cannot update container, target container already exists.");
+        logger.error("containerUpdate: No values changed, aborting.")
     }
 }
 
 /** Gets a container object from a supplied container id string.
- *  @param containerId  expects a single digit string.
+ *  @param containerId Expects a cookieStringId e.g. "firefox-container-n"
  */
 export async function containerGetFromId(containerId: string): Promise<{}> {
-    let container = await browser.contextualIdentities.get(
-        "firefox-container-" + containerId,
-    )
-    return container
+    return await browser.contextualIdentities.get(containerId)
 }
 
 /** Queries Firefox's contextual identities API for a container with a specific
  *  name. The color and icon are then compared. This is done to impose a unique
- *  constraint on those parameters. 
+ *  constraint on those parameters.
  *  The function returns true if all the parameters have not been
  *  supplied to ensure that checks for uniqueness can't be circumvented.
  *  @params container
@@ -79,36 +80,39 @@ export async function containerExists(container: {
 }): Promise<boolean> {
     let exists = false
     try {
-        let res = await browser.contextualIdentities.query({ name: container.name })
+        let res = await browser.contextualIdentities.query({
+            name: container.name,
+        })
         if (res.length > 0) {
             for (let c of res) {
                 if (c.color === container.color && c.icon === container.icon)
                     exists = true
             }
-        } 
-    }
-    catch (e) {
-        exists = true // Makes sure we don't accidentally break the constraint on query error.
-        logger.debug(e)
+        }
+    } catch (e) {
+        exists = true // Make sure we don't accidentally break the constraint on query error.
+        logger.error("Error querying contextualIdentities:", e)
     }
     return exists
 }
 
-/** Takes string parameters and returns them as a pseudo container object 
+/** Takes string parameters and returns them as a pseudo container object
  *  for use in otherfunctions in the library.
  *  @params name
  *  @params color
  *  @params icon
  */
 export function containerFromString(name: string, color: string, icon: string) {
-    return { 
+    return {
         name: name,
         color: color as browser.contextualIdentities.IdentityColor,
         icon: icon as browser.contextualIdentities.IdentityIcon,
     }
 }
 
-/** Returns all containers in an array */
+/** Returns an array representation of all containers.
+ *  Something something completions?
+ */
 export async function containerGetAll(): Promise<any[]> {
-    return await browser.contextualIdentities.query({});
+    return await browser.contextualIdentities.query({})
 }
