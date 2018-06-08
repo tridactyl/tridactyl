e710296d7003c1fa2d9b13fa7cd427e90ca5c0f2
Colin Caine <cmc@cs.man.ac.uk>
2018-06-08 13:34:41 +0100

    Fix authors not appearing when run non-interactively
    git shortlog behaves differently when it's run non-interactively.

--- scripts/authors.sh
+++ scripts/authors.sh
@@ -9,5 +9,5 @@ cd src/static
 authors="../../build/static/authors.html"
 
 sed "/REPLACETHIS/,$ d" authors.html > "$authors"
-git shortlog -sn | cut -c8- | sed 's/^/<p>/' | sed 's/$/<\/p>/' >> "$authors"
+git shortlog -sn HEAD | cut -c8- | sed 's/^/<p>/' | sed 's/$/<\/p>/' >> "$authors"
 sed "1,/REPLACETHIS/ d" authors.html >> "$authors"
