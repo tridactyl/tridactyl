f041dbd25b791fb6e017dcad0dd6127dc930cd32
glacambre <code@lacamb.re>
2018-06-30 13:35:35 +0200

    {excmds,webext,messaging}.ts: Remove l() promise logger
    When a promise throws an error in l(), the error is automatically logged
    in the console. This is a problem because it happens even when the error
    is catched by the code that called l(), which might not want to log
    errors at all.
    
    l() was necessary when errors thrown in promises didn't reach the
    toplevel. Now that they do, it is safe to remove l().
    
    This will enable better error handling.

--- src/excmds.ts
+++ src/excmds.ts
@@ -87,7 +87,7 @@
 
 // Shared
 import * as Messaging from "./messaging"
-import { l, browserBg, activeTabId, activeTabContainerId } from "./lib/webext"
+import { browserBg, activeTabId, activeTabContainerId } from "./lib/webext"
 import * as Container from "./lib/containers"
 import state from "./state"
 import * as UrlUtil from "./url_util"
@@ -1244,7 +1244,7 @@ export async function zoom(level = 0, rel = "false") {
  */
 //#background
 export async function reader() {
-    if (await l(firefoxVersionAtLeast(58))) {
+    if (await firefoxVersionAtLeast(58)) {
         let aTab = await activeTab()
         if (aTab.isArticle) {
             browser.tabs.toggleReaderMode()
@@ -1569,15 +1569,13 @@ async function idFromIndex(index?: number | "%" | "#" | string): Promise<number>
     } else if (index !== undefined && index !== "%") {
         // Wrap
         index = Number(index)
-        index = (index - 1).mod((await l(browser.tabs.query({ currentWindow: true }))).length) + 1
+        index = (index - 1).mod((await browser.tabs.query({ currentWindow: true })).length) + 1
 
         // Return id of tab with that index.
-        return (await l(
-            browser.tabs.query({
-                currentWindow: true,
-                index: index - 1,
-            }),
-        ))[0].id
+        return (await browser.tabs.query({
+            currentWindow: true,
+            index: index - 1,
+        }))[0].id
     } else {
         return await activeTabId()
     }
