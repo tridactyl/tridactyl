5d7e8aba784c16852addaab8f65c5276905bba5d
WorldCodeCentral <alexander.kern@alumni.fh-aachen.de>
2018-07-14 20:38:48 +0200

    Update History.ts
    Correct mistake that tabopen -b took two arguments when it should take only one

--- src/completions/History.ts
+++ src/completions/History.ts
@@ -69,8 +69,8 @@ export class HistoryCompletionSource extends Completions.CompletionSourceFuse {
             }
             if (query.startsWith("-b")) {
                 let args = query.split(" ")
-                options = args.slice(0, 2).join(" ")
-                query = args.slice(2).join(" ")
+                options = args.slice(0, 1).join(" ")
+                query = args.slice(1).join(" ")
             }
         } else if (prefix == "winopen ") {
             if (query.startsWith("-private")) {
