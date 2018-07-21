tridactyl/tridactyl

commit e5d2be73eb06a4506755ddbc17451a672a451d0d

Fix #838: remove errant space

src/completions/History.ts
@@ -80,7 +80,7 @@ export class HistoryCompletionSource extends Completions.CompletionSourceFuse {
         }
 
         this.options = (await this.scoreOptions(query, 10)).map(
-            page => new HistoryCompletionOption(options + " " + page.url, page),
+            page => new HistoryCompletionOption(options + page.url, page),
         )
 
         this.updateChain()

commit 7025c9e2fc64262850369549345781aed9156372

History.ts: Add missing space when inserting completion options
e5d2be7 introduced a bug while trying to fix
https://github.com/cmcaine/tridactyl/issues/838: when the command line
contains `tabopen -flag ` and the user tries to insert the selected
completion option in the command line by pressing <Space>, the resulting
command line will be `tabopen -flagurl` instead of `tabopen -flag url`.

This is fixed by appending a trailing space to the `options` variable if
it isn't an empty string.

src/completions/History.ts
@@ -78,6 +78,7 @@ export class HistoryCompletionSource extends Completions.CompletionSourceFuse {
                 query = query.substring(options.length)
             }
         }
+        options += options ? " " : ""
 
         this.options = (await this.scoreOptions(query, 10)).map(
             page => new HistoryCompletionOption(options + page.url, page),