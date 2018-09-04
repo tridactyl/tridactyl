tridactyl/tridactyl

commit 14d7acc5313a3bca6a709a21ae5526c8a46893da

Make config docs a bit useful

src/config.ts
@@ -31,9 +31,9 @@ function schlepp(settings) {
 
 // TODO: have list of possibilities for settings, e.g. hintmode: reverse | normal
 let USERCONFIG = o({})
-const DEFAULTS = o({
+const _DEFAULTS = {
     configversion: "0.0",
-    nmaps: o({
+    nmaps: {
         "<F1>": "help",
         o: "fillcmdline open",
         O: "current_url open",
@@ -132,13 +132,19 @@ const DEFAULTS = o({
         ".": "repeat",
         "<SA-ArrowUp><SA-ArrowUp><SA-ArrowDown><SA-ArrowDown><SA-ArrowLeft><SA-ArrowRight><SA-ArrowLeft><SA-ArrowRight>ba":
             "open https://www.youtube.com/watch?v=M3iOROuTuMA",
-    }),
-    autocmds: o({
-        DocStart: o({
+    },
+    autocmds: {
+        DocStart: {
             "addons.mozilla.org": "mode ignore",
-        }),
-    }),
-    exaliases: o({
+        },
+    },
+
+    /**
+     * Aliases for use in the command line. The key is replaced with the value.
+     *
+     * Related ex command: `command`
+     */
+    exaliases: {
         alias: "command",
         au: "autocmd",
         b: "buffer",
@@ -169,13 +175,13 @@ const DEFAULTS = o({
         openwith: "hint -W",
         "!": "exclaim",
         "!s": "exclaim_quiet",
-    }),
-    followpagepatterns: o({
-        next: "^(next|newer)\\b|»|>>|more",
-        prev: "^(prev(ious)?|older)\\b|«|<<",
-    }),
+    },
+    followpagepatterns: {
+        next: "^(next|newer\\b|»|>>|more",
+        prev: "^(prev(ious?|older\\b|«|<<",
+    },
     searchengine: "google",
-    searchurls: o({
+    searchurls: {
         google: "https://www.google.com/search?q=",
         scholar: "https://scholar.google.com/scholar?q=",
         googleuk: "https://www.google.co.uk/search?q=",
@@ -199,7 +205,7 @@ const DEFAULTS = o({
         gentoo_wiki:
             "https://wiki.gentoo.org/index.php?title=Special%3ASearch&profile=default&fulltext=Search&search=",
         qwant: "https://www.qwant.com/?q=",
-    }),
+    },
 
     newtab: "",
     viewsource: "tridactyl", // "tridactyl" or "default"
@@ -210,7 +216,7 @@ const DEFAULTS = o({
 
     // Controls whether the page can focus elements for you via js
     // Remember to also change browser.autofocus (autofocusing elements via
-    // HTML) in about:config
+    // HTML in about:config
     // Maybe have a nice user-vicible message when the setting is changed?
     allowautofocus: "true",
 
@@ -234,21 +240,21 @@ const DEFAULTS = o({
     modeindicator: "true",
 
     // Default logging levels - 2 === WARNING
-    logging: o({
+    logging: {
         messaging: 2,
         cmdline: 2,
         controller: 2,
         hinting: 2,
         state: 2,
         excmd: 1,
-    }),
+    },
     noiframeon: [],
 
     // Native messenger settings
     // This has to be a command that stays in the foreground for the whole editing session
     // "auto" will attempt to find a sane editor in your path.
     // Please send your requests to have your favourite terminal moved further up the list to /dev/null.
-    //          (but we are probably happy to add your terminal to the list if it isn't already there).
+    //          (but we are probably happy to add your terminal to the list if it isn't already there.
     editorcmd: "auto",
     browser: "firefox",
     nativeinstallcmd:
@@ -263,7 +269,8 @@ const DEFAULTS = o({
     // Security settings
 
     csp: "untouched", // change this to "clobber" to ruin the CSP of all sites and make Tridactyl run a bit better on some of them, e.g. raw.github*
-})
+}
+const DEFAULTS = o(_DEFAULTS)
 
 /** Given an object and a target, extract the target if it exists, else return undefined
 

src/static/typedoc/assets/css/main.css
@@ -937,6 +937,15 @@ ul.tsd-descriptions h4 {
 
 }
 
+.tsd-parent-kind-object-literal > h3, .tsd-sources {
+    display: none
+}
+
+.tsd-parent-kind-object-literal > .tsd-signature {
+    border: none
+}
+
+
 
 /* Other changes:
  * - removed all background images in CSS above

commit 0fcef90a78c3d99a8b5132aa9e81ef1bfdee1ecc

config.ts: Fix followpagepatterns
14d7acc5 accidentally broke the regexes used by followpage. This commit
fixes them.

src/config.ts
@@ -371,8 +371,8 @@ class default_config {
      * Edit these if you want to add, e.g. other language support.
      */
     followpagepatterns = {
-        next: "^(next|newer\\b|»|>>|more",
-        prev: "^(prev(ious?|older\\b|«|<<",
+        next: "^(next|newer)\\b|»|>>|more",
+        prev: "^(prev(ious)?|older)\\b|«|<<",
     }
 
     /**