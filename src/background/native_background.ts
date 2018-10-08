fe62ebe96b7a77d35751b477dc195e0ae5bd666b
Joao Sa <me@joaomsa.com>
2018-10-08 23:56:35 +0000

    Allow control over filename position in editorcmd

--- src/background/native_background.ts
+++ src/background/native_background.ts
@@ -106,10 +106,9 @@ export async function getBestEditor(): Promise<string> {
             "urxvt -e",
             "alacritty -e", // alacritty is nice but takes ages to start and doesn't support class
             "cool-retro-term -e",
-            // Terminator and termite require  -e commands to be in quotes, hence the extra quote at the end.
-            // The closing quote is implemented in the editor function.
-            'terminator -e "',
-            'termite --class tridactyl_editor -e "',
+            // Terminator and termite require  -e commands to be in quotes
+            'terminator -e "{}"',
+            'termite --class tridactyl_editor -e "{}"',
             "dbus-launch gnome-terminal --",
             // I wanted to put hyper.js here as a joke but you can't start it running a command,
             // which is a far better joke: a terminal emulator that you can't send commands to.
@@ -223,11 +222,11 @@ export async function editor(file: string, content?: string) {
         config.get("editorcmd") == "auto"
             ? await getBestEditor()
             : config.get("editorcmd")
-    // Dirty hacks for termite and terminator support part 2.
-    const e = editorcmd.split(" ")[0]
-    if (e === "termite" || e === "terminator") {
-        await run(editorcmd + " " + file + '"')
-    } else await run(editorcmd + " " + file)
+    if (editorcmd.indexOf("{}") != -1) {
+        await run(editorcmd.replace(/{}/, file))
+    } else {
+        await run(editorcmd + " " + file)
+    }
     return await read(file)
 }
 
