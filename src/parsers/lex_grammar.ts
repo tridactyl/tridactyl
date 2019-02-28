eafec3121916ec0a900223abd4afaed7f510054a
Isaac Khor <s.github@isaackhor.com>
2019-02-28 13:01:05 -0500

    Revise lexical grammar

--- src/parsers/lex_grammar.ts
+++ src/parsers/lex_grammar.ts
@@ -3,18 +3,23 @@ import { Token, makeLexer } from "@src/parsers/scanner"
 // Define a basic lexical grammar
 
 const lexicalGrammar: Token[] = [
-    { type: "cmdname", pattern: /^\S+/ },
     // Put multi before single to match it first
     { type: "shortArgMulti", pattern: /^-[a-zA-Z]+/ },
     { type: "shortArgSingle", pattern: /^-[a-zA-Z]/ },
     // The regex matches as many non escaped quotes as possible until
     // the next quote
-    { type: "stringLit", pattern: /^"(?:\\"|[^"])*?"/ },
+    {
+        type: "stringLit",
+        pattern: /^"(?:\\"|[^"])*?"/,
+        // Offload string literal parsing to JSON parser
+        processor: JSON.parse,
+    },
     {
         type: "numLit",
         pattern: /^\d+(?:\.\d+)?/,
-        processor: Number.parseInt,
+        processor: Number.parseFloat,
     },
+    { type: "argument", pattern: /^\S+/ },
 ]
 
 export const exmodeScanner = makeLexer(lexicalGrammar)
