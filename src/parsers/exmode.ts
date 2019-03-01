e7bbdee72d93bbf4689f562f3e6ae8e4de657e42
Isaac Khor <s.github@isaackhor.com>
2019-02-28 23:43:02 -0500

    Delete old parser

--- src/parsers/exmode.ts
+++ src/parsers/exmode.ts
@@ -9,43 +9,8 @@ import { enumerate, head, izip } from "@src/lib/itertools"
 import { exmodeScanner } from "@src/parsers/lex_grammar"
 const logger = new Logging.Logger("exmode")
 
-/* Converts numbers, boolean, string[].
-
-   string[] eats all remaining arguments, so it should only be used as a
-   type of the last arg.
-
-   TODO: quoted strings
-   TODO: shell style options
-   TODO: counts
-*/
-function convertArgs(params, argv) {
-    const conversions = {
-        number: convert.toNumber,
-        boolean: convert.toBoolean,
-        string: s => s,
-        ModeName: s => s,
-    }
-
-    const typedArgs = []
-    let type, arg, i
-    for ([type, [i, arg]] of izip(params.values(), enumerate(argv))) {
-        if (type in conversions) {
-            typedArgs.push(conversions[type](arg))
-        } else if (type.includes("|") || ["'", '"'].includes(type[0])) {
-            // Do your own conversions!
-            typedArgs.push(arg)
-        } else if (type === "string[]") {
-            // Eat all remaining arguments
-            return [...typedArgs, ...argv.slice(i)]
-        } else throw new TypeError(`Unknown type: ${type}`)
-    }
-    return typedArgs
-}
-
 // Simplistic Ex command line parser.
-// TODO: Quoting arguments
 // TODO: Pipe to separate commands
-// TODO: Abbreviated commands
 export function parser(exstr: string): any[] {
     // Expand aliases
     const expandedExstr = aliases.expandExstr(exstr)
