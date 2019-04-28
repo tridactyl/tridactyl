e49dbd5d21fe99286061f4fa8543815e017fd89a
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2019-04-27 23:09:51 -0700

    Add typing information in exmcds_lib to reduce copy-paste

--- src/excmds/fillcmdline.ts
+++ src/excmds/fillcmdline.ts
@@ -1,6 +1,4 @@
 import * as excmd_lib from "@src/lib/excmd_lib"
 import * as _excmds from "@src/excmds/_fillcmdline"
-type ft = typeof _excmds
-type ArgumentsType<T> = T extends (...args: infer U) => any ? U: never;
-export default excmd_lib.forwardedToContent("content_excmds/fillcmdline", _excmds) as {
-    [k in keyof ft]: (...args: ArgumentsType<ft[k]>) => ReturnType<ft[k]> }
+export default excmd_lib.forwardedToContent("content_excmds/fillcmdline", _excmds)
+
