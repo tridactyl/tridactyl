e5089676c8dcd7927ea94d9be9d47231ad1b8d19
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2019-04-27 23:09:51 -0700

    Add typing information in exmcds_lib to reduce copy-paste

--- src/excmds/open.ts
+++ src/excmds/open.ts
@@ -1,6 +1,3 @@
 import * as excmd_lib from "@src/lib/excmd_lib"
 import * as _excmds from "@src/excmds/_open"
-type ft = typeof _excmds
-type ArgumentsType<T> = T extends (...args: infer U) => any ? U: never;
-export default excmd_lib.forwardedToBackground("background_excmds/open", _excmds) as {
-    [k in keyof ft]: (...args: ArgumentsType<ft[k]>) => ReturnType<ft[k]> }
+export default excmd_lib.forwardedToBackground("background_excmds/open", _excmds)
