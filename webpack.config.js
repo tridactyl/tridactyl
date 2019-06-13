a24487786a0630caeecf0d055d58ebd0f8b56584
Oliver Blanthorn <freedom4cows@gmail.com>
2019-06-13 19:40:37 +0100

    Add initial steps towards Chrome support
    Build using `yarn run build` and then
    `eval (yarn bin)/webpack --browser chrome`. Load into
    chrome://extensions by toggling Developer Mode and then
    'Load unpacked' the build/ directory.
    
    What works:
    
    - the options page
    
    - simple commands on the options page
    
    What doesn't work:
    
    - our iframe is loaded into other pages but we don't capture
    keyevents
    
    - new tab page
    
    - most ex-commands on the options page (we often hit errors
    due to using Firefox features that aren't in Chrome)
    
    - styling on the command line
    
    We clearly need a wrapper for the `browser` API that fails more
    gracefully where something isn't available.

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,21 @@
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
+const { TsConfigPathsPlugin } = require("awesome-typescript-loader")
 const CopyWebPackPlugin = require("copy-webpack-plugin")
+const webpack = require("webpack")
+// const WebpackShellPlugin = require('webpack-shell-plugin')
 
-const fileExtensions = [".ts", ".tsx", ".js", ".json"]
-
-module.exports = {
-
-    mode: "development",
-    // mode: "production", // Uncomment me for more helpful error messages
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-    // devtool: "inline-source-map", // Uncomment me for more helpful error messages
-
-    entry: {
-        background: "./src/background.ts",
-        content: "./src/content.ts",
-        commandline_frame: "./src/commandline_frame.ts",
-        help: "./src/help.ts",
-        newtab: "./src/newtab.ts",
-    },
-    output: {
-        filename: "[name].js",
-        path: __dirname + "/build",
-    },
-
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: fileExtensions,
-        plugins: [new TsconfigPathsPlugin({extensions: fileExtensions})]
-    },
-
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-        ],
-    },
-
-    plugins: [
+module.exports = (env, argv) => {
+    let plugins = [
+        // new UglifyJSPlugin({
+        //     uglifyOptions: {
+        //         ecma: 8
+        //     }
+        // }),
+        // new WebpackShellPlugin({onBuildStart: [
+        //     'mkdir -p generated/static',
+        //     'scripts/excmds_macros.py',
+        //     'scripts/newtab.md.sh',
+        //     'scripts/make_docs.sh',
+        // ]}),
         new CopyWebPackPlugin([
             { from: "src/manifest.json" },
             {
@@ -52,10 +26,51 @@ module.exports = {
             { from: "generated/static", to: "static" },
             { from: "issue_template.md" },
         ]),
-    ],
-    // Fix css
-    // https://github.com/webpack-contrib/css-loader/issues/447#issuecomment-285598881
-    node: {
-        fs: "empty",
-    },
+    ]
+    if (argv.browser === 'chrome') {
+        plugins.push(
+            new webpack.ProvidePlugin({
+                browser: 'webextension-polyfill-for-webpack',
+            }),
+        )
+    }
+    return {
+        mode: "development",
+        entry: {
+            background: "./src/background.ts",
+            content: "./src/content.ts",
+            commandline_frame: "./src/commandline_frame.ts",
+            help: "./src/help.ts",
+            newtab: "./src/newtab.ts",
+        },
+        output: {
+            filename: "[name].js",
+            path: __dirname + "/build",
+        },
+
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: "inline-source-map",
+
+        resolve: {
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: [".ts", ".tsx", ".js", ".json"],
+            plugins: [new TsConfigPathsPlugin()],
+        },
+
+        module: {
+            rules: [
+                // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+                { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            ],
+        },
+        plugins,
+        // Fix css
+        // https://github.com/webpack-contrib/css-loader/issues/447#issuecomment-285598881
+        node: {
+            fs: "empty",
+        },
+    }
 }
