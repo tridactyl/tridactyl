16194dacd7dfc018b598257b1cc1eac40e53c0ae
Colin Caine <cmc@cs.man.ac.uk>
2019-06-14 18:44:58 +0100

    nativeopen: fix shell escaping

--- src/excmds.ts
+++ src/excmds.ts
@@ -84,6 +84,7 @@ import Mark from "mark.js"
 import * as CSS from "css"
 import * as Perf from "@src/perf"
 import * as Metadata from "@src/.metadata.generated"
+import * as escape from "@src/lib/escape"
 
 /** @hidden **/
 const TRI_VERSION = "REPLACE_ME_WITH_THE_VERSION_USING_SED"
@@ -462,7 +463,12 @@ export async function fixamo() {
 /**
  * Uses the native messenger to open URLs.
  *
- * **Be *seriously* careful with this: you can use it to open any URL you can open in the Firefox address bar.**
+ * **Be *seriously* careful with this:**
+ *
+ * 1. the implementation basically execs `firefox --new-tab <your shell escaped string here>`
+ * 2. you can use it to open any URL you can open in the Firefox address bar,
+ *    including ones that might cause side effects (firefox does not guarantee
+ *    that about: pages ignore query strings).
  *
  * You've been warned.
  */
@@ -498,7 +504,20 @@ export async function nativeopen(url: string, ...firefoxArgs: string[]) {
                 await Native.run("osascript " + osascriptArgs.join(" ") + " " + url)
             } else {
                 if (firefoxArgs.length === 0) firefoxArgs = ["--new-tab"]
-                await Native.run(config.get("browser") + " " + firefoxArgs.join(" ") + " " + url)
+
+                const os = (await browser.runtime.getPlatformInfo()).os
+                let escapedUrl: string
+                // We need to quote and escape single quotes in the
+                // url, otherwise an attacker could create an anchor with a url
+                // like 'file:// && $(touch /tmp/dead)' and achieve remote code
+                // execution when the user tries to follow it with `hint -W tabopen`
+                if (os === "win") {
+                    escapedUrl = escape.windows_cmd(url)
+                } else {
+                    escapedUrl = escape.sh(url)
+                }
+
+                await Native.run(`${config.get("browser")} ${firefoxArgs.join(" ")} ${escapedUrl}`)
             }
             setTimeout(() => browser.tabs.onCreated.removeListener(selecttab), 100)
         } catch (e) {
