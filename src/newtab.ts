10b6922e62bfadb635433a913911fb183e547cc1
Oliver Blanthorn <freedom4cows@gmail.com>
2019-07-11 11:33:04 +0100

    Add scary messages to new tab and tutor
    Trying to persuade people to update and don't have time to
    backport to this version.

--- src/newtab.ts
+++ src/newtab.ts
@@ -3,47 +3,6 @@
 import * as Messaging from "@src/lib/messaging"
 import * as config from "@src/lib/config"
 
-// These functions work with the elements created by tridactyl/scripts/newtab.md.sh
-function getChangelogDiv() {
-    const changelogDiv = document.getElementById("changelog")
-    if (!changelogDiv) throw new Error("Couldn't find changelog element!")
-    return changelogDiv
-}
-
-function updateChangelogStatus() {
-    const changelogDiv = getChangelogDiv()
-    const changelogContent = changelogDiv.textContent
-    if (localStorage.changelogContent === changelogContent) {
-        const changelogButton = document.querySelector('input[id^="spoiler"]')
-        if (!changelogButton) {
-            console.error("Couldn't find changelog button!")
-            return
-        }
-        changelogButton.classList.add("seen")
-    }
-}
-
-function readChangelog() {
-    const changelogDiv = getChangelogDiv()
-    localStorage.changelogContent = changelogDiv.textContent
-    updateChangelogStatus()
-}
-
-window.addEventListener("load", updateChangelogStatus)
-window.addEventListener("load", _ => {
-    const spoilerbutton = document.getElementById("spoilerbutton")
-    if (!spoilerbutton) {
-        console.error("Couldn't find spoiler button!")
-        return
-    }
-    spoilerbutton.addEventListener("click", readChangelog)
-    config.getAsync("newtabfocus").then(f => {
-        if (f === "page") {
-            window.focus()
-        }
-    })
-})
-
 // Periodically nag people about updates.
 window.addEventListener("load", _ => {
     if (config.get("update", "nag") === true) {
