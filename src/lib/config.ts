976377a843cd8b9b85738d7bc2e39c3a14204d60
Oliver Blanthorn <freedom4cows@gmail.com>
2020-04-25 10:41:25 +0100

    Try to fix mergeDeep null handling: #2324

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1071,6 +1071,7 @@ function setDeepProperty(obj, value, target) {
  * Merges two objects and any child objects they may have
  */
 export function mergeDeep(o1, o2) {
+    if (o1 === null) return o2
     const r = Array.isArray(o1) ? o1.slice() : Object.create(o1)
     Object.assign(r, o1, o2)
     if (o2 === undefined) return r
@@ -1078,7 +1079,7 @@ export function mergeDeep(o1, o2) {
         .filter(
             key => typeof o1[key] === "object" && typeof o2[key] === "object",
         )
-        .forEach(key => Object.assign(r[key], mergeDeep(o1[key], o2[key])))
+        .forEach(key => r[key] == null ? null : Object.assign(r[key], mergeDeep(o1[key], o2[key])))
     return r
 }
 
