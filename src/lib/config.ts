37ea90f584f7a8e368b049b07594380a5575091a
Oliver Blanthorn <freedom4cows@gmail.com>
2020-05-18 18:03:55 +0100

    Ensure mergeDeep always returns a copy

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1087,7 +1087,7 @@ function setDeepProperty(obj, value, target) {
  * Merges two objects and any child objects they may have
  */
 export function mergeDeep(o1, o2) {
-    if (o1 === null) return o2
+    if (o1 === null) return Object.assign({}, o2)
     const r = Array.isArray(o1) ? o1.slice() : Object.create(o1)
     Object.assign(r, o1, o2)
     if (o2 === undefined) return r
