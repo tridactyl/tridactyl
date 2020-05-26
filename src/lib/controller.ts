b5d7fb00fe8ca804dacb1df9f1c05e91a8e745f1
Oliver Blanthorn <freedom4cows@gmail.com>
2020-05-26 16:07:16 +0100

    Fix #2424 - never store `winopen -private` for repeat

--- src/lib/controller.ts
+++ src/lib/controller.ts
@@ -15,8 +15,8 @@ export async function acceptExCmd(exstr: string): Promise<any> {
     // TODO: Errors should go to CommandLine.
     try {
         const [func, args] = exmode_parser(exstr, stored_excmds)
-        // Stop the repeat excmd from recursing.
-        if (func !== stored_excmds[""].repeat) {
+        // Stop the repeat excmd from recursing and don't store private window commands
+        if (func !== stored_excmds[""].repeat && exstr.search("winopen -private") < 0) {
             State.getAsync("last_ex_str").then(last_ex_str => {
                 if (last_ex_str != exstr) state.last_ex_str = exstr
             })
