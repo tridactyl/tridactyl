63622c90ff0363266c5d075c8ce19904b7c6c5d6
Oliver Blanthorn <freedom4cows@gmail.com>
2020-06-29 14:59:44 +0100

    Tighten parser types
    daa65adcc8dde7ef9a3c61dd43ec64b9ff3e2b6b has sent me on a crusade to
    banish `: any` types as far as possible

--- src/parsers/nmode.ts
+++ src/parsers/nmode.ts
@@ -25,7 +25,7 @@ export function init(endCommand: string, mode = "normal", numCommands = 1) {
 }
 
 /** Receive keypress. If applicable, execute a command. */
-export function parser(keys: KeyboardEvent[]) {
+export function parser(keys: keyseq.KeyEventLike[]) {
     keys = keyseq.stripOnlyModifiers(keys)
     if (keys.length === 0) return { keys: [], isMatch: false }
     const conf = mode2maps.get(modeState.mode) || modeState.mode + "maps"
