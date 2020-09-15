8bb32ed0aa9cfc1d613ff031fc290db796fc0319
Simon H Moore <simon@simonhugh.xyz>
2020-09-15 22:40:00 +0100

    Add functions for getting window index and id

--- src/lib/webext.ts
+++ src/lib/webext.ts
@@ -74,6 +74,18 @@ export async function ownTabId() {
     return (await ownTab()).id
 }
 
+async function windows () { return (await browser.windows.getAll()).map(w => w.id).sort((a, b) => a - b) }
+
+/* Returns tridactyls window index. */
+export async function ownWinTriIndex() {
+    return (await windows()).indexOf((await ownTab()).windowId)
+}
+
+/* Returns mozilla's internal window id from tridactyls index. */
+export async function getWinIdFromIndex(index: string) {
+    return (await windows())[index]
+}
+
 export async function ownTabContainer() {
     return browserBg.contextualIdentities.get((await ownTab()).cookieStoreId)
 }
