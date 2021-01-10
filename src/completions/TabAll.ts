c0df3683bdf253ee32bb76d7ece4d9ebee36709f
Oliver Blanthorn <freedom4cows@gmail.com>
2021-01-10 12:52:12 +0100

    Refresh taball completions on tab changes

--- src/completions/TabAll.ts
+++ src/completions/TabAll.ts
@@ -2,10 +2,13 @@ import * as Perf from "@src/perf"
 import { browserBg } from "@src/lib/webext"
 import * as Containers from "@src/lib/containers"
 import * as Completions from "@src/completions"
+import * as Messaging from "@src/lib/messaging"
 
-class TabAllCompletionOption extends Completions.CompletionOptionHTML
+class TabAllCompletionOption
+    extends Completions.CompletionOptionHTML
     implements Completions.CompletionOptionFuse {
     public fuseKeys = []
+    public tab: browser.tabs.Tab
     constructor(
         public value: string,
         tab: browser.tabs.Tab,
@@ -16,6 +19,7 @@ class TabAllCompletionOption extends Completions.CompletionOptionHTML
         super()
         this.value = `${winindex}.${tab.index + 1}`
         this.fuseKeys.push(this.value, tab.title, tab.url)
+        this.tab = tab
 
         // Create HTMLElement
         const favIconUrl = tab.favIconUrl
@@ -46,6 +50,8 @@ export class TabAllCompletionSource extends Completions.CompletionSourceFuse {
 
         this.updateOptions()
         this._parent.appendChild(this.node)
+
+        Messaging.addListener("tab_changes", () => this.reactToTabChanges())
     }
 
     async onInput(exstr) {
@@ -65,6 +71,48 @@ export class TabAllCompletionSource extends Completions.CompletionSourceFuse {
         windows.forEach(win => (response[win.id] = win))
         return response
     }
+
+    /**
+     * Update the list of possible tab options and select (focus on)
+     * the appropriate option.
+     */
+    private async reactToTabChanges(): Promise<void> {
+        // const prevOptions = this.options
+        await this.updateOptions(this.lastExstr)
+
+        // TODO: update this from Tab.ts for TabAll.ts
+        // if (!prevOptions || !this.options || !this.lastFocused) return
+
+        // // Determine which option to focus on
+        // const diff = R.differenceWith(
+        //     (x, y) => x.tab.id === y.tab.id,
+        //     prevOptions,
+        //     this.options,
+        // )
+        // const lastFocusedTabCompletion = this
+        //     .lastFocused as TabAllCompletionOption
+
+        // // If the focused option was removed then focus on the next option
+        // if (
+        //    diff.length === 1 &&
+        //    diff[0].tab.id === lastFocusedTabCompletion.tab.id
+        // ) {
+        //    //this.select(this.getTheNextTabOption(lastFocusedTabCompletion))
+        // }
+    }
+
+    /**
+     * Gets the next option in this BufferCompletionSource assuming
+     * that this BufferCompletionSource length has been reduced by 1
+     *
+     * TODO: this ain't going to work, need to work out position based on win.tab
+     */
+    // private getTheNextTabOption(option: TabAllCompletionOption) {
+    //     if (option.tab.index === this.options.length) {
+    //         return this.options[this.options.length - 1]
+    //     }
+    //     return this.options[option.tab.index]
+    // }
 
     // Eslint doesn't like this decorator but there's nothing we can do about it
     // eslint-disable-next-line @typescript-eslint/member-ordering
