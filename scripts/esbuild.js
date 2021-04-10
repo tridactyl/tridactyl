f75f81f49e227a62ac9ccdab6913397d9dd322db
Oliver Blanthorn <freedom4cows@gmail.com>
2021-04-10 22:58:02 +0200

    Add sourcemaps

--- scripts/esbuild.js
+++ scripts/esbuild.js
@@ -4,6 +4,7 @@ for (let f of ["content", "background", "help", "newtab", "commandline_frame"]) 
         esbuild.build({
         entryPoints: [`src/${f}.ts`],
         bundle: true,
+        sourcemap: true,
         target: "firefox68",
         outfile: `build/${f}.js`,
     }).catch(() => process.exit(1))
