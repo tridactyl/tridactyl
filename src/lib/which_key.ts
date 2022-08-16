10eefd1bab035ec4b73e1f23a56f4b4d269c4a57
Martti <martti@aukia.com>
2022-08-16 16:06:13 +0300

    Add different css class for non-terminal whichkey options

--- src/lib/which_key.ts
+++ src/lib/which_key.ts
@@ -4,22 +4,26 @@ import * as Messaging from "@src/lib/messaging"
 
 const logger = new Logger("whichkey")
 
+export type WhichKeyMap = Map<string, { target: string; isTerm: boolean }>
+
 /** Display possible suffixes with their respective targets */
 export function whichKey(prefix: string, possibleMappings?: KeyMap) {
     if (!possibleMappings || possibleMappings.size === 0) {
         return
     }
 
-    const maps: Map<string, string> = new Map()
+    const maps: WhichKeyMap = new Map()
     for (let [key, target] of possibleMappings) {
         const suffix = key.slice(prefix.length)
         // if suffix has multiple terminators, only show multiple suffixes
+        let isTerm = true
         if (suffix.length > 1) {
             // TODO: target should be setable by user
             target = "+prefix"
+            isTerm = false
         }
         const printableSuffix = suffix[0].toMapstr()
-        maps.set(printableSuffix, target as string)
+        maps.set(printableSuffix, { target: target as string, isTerm })
     }
     logger.debug(maps)
     Messaging.messageOwnTab("whichkey_content", "show")
