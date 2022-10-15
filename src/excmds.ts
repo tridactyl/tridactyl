6a10ae83d9ffb93ebd6b4a5447e072c6b8844cf5
Michael Joseph Rosenthal <rosenthalm93@gmail.com>
2022-10-15 09:03:22 -0700

    Add link in help native and help nativeinstall

--- src/excmds.ts
+++ src/excmds.ts
@@ -719,6 +719,7 @@ export async function exclaim_quiet(...str: string[]) {
 /**
  * Tells you if the native messenger is installed and its version.
  *
+ * For snap, flatpak, and other sandboxed installations, additional setup is required – see https://github.com/tridactyl#extra-features-through-native-messaging.
  */
 //#background
 export async function native() {
@@ -738,6 +739,8 @@ export async function native() {
  * The native messenger's source code may be found here: https://github.com/tridactyl/native_messenger/blob/master/src/native_main.nim
  *
  * If your corporate IT policy disallows execution of binaries which have not been whitelisted but allows Python scripts, you may instead use the old native messenger by running `install.sh` or `win_install.ps1` from https://github.com/tridactyl/tridactyl/tree/master/native - the main downside is that it is significantly slower.
+ * 
+ * For snap, flatpak, and other sandboxed installations, additional setup is required – see https://github.com/tridactyl#extra-features-through-native-messaging.
  */
 //#background
 export async function nativeinstall() {
