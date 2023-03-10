c692bff414cce990b0f7ae2919f064f22d5b1e10
Oliver Blanthorn <freedom4cows@gmail.com>
2023-03-10 23:15:51 +0100

    Fix typedoc entrypoint

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dest=generated/static/docs
-"$(yarn bin)/typedoc" --skipErrorChecking --theme src/static/typedoc/ --exclude "src/**/?(test_utils|*.test).ts"  --out $dest src
+"$(yarn bin)/typedoc" --entryPointStrategy expand --skipErrorChecking --theme src/static/typedoc/ --exclude "src/**/?(test_utils|*.test).ts"  --out $dest src
 cp -r $dest build/static/
