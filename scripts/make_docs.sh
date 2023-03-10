0c103f2df9de3627742153ce0fe51ba30bc44495
Oliver Blanthorn <freedom4cows@gmail.com>
2023-03-10 22:29:17 +0100

    Broken: update TypeScript, typedoc and jest
    It looks like TypeScript 4 might have broken our metadata generation,
    which isn't that surprising I guess.

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dest=generated/static/docs
-"$(yarn bin)/typedoc" --theme src/static/typedoc/ --exclude "src/**/?(test_utils|*.test).ts"  --out $dest src --ignoreCompilerErrors
+"$(yarn bin)/typedoc" --skipErrorChecking --theme src/static/typedoc/ --exclude "src/**/?(test_utils|*.test).ts"  --out $dest src
 cp -r $dest build/static/
