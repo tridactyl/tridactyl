fb5a0128e60626b27cb2d8c4304c6ae878fa27b8
Oliver Blanthorn <freedom4cows@gmail.com>
2023-04-05 20:45:03 +0200

    Add reader in new tab bind

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -283,6 +283,7 @@ export class default_config {
         g$: "tablast",
         ga: "tabaudio",
         gr: "reader",
+        gR: "reader --tab",
         gu: "urlparent",
         gU: "urlroot",
         gf: "viewsource",
