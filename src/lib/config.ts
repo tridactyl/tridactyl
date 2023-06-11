761d71b99e3008e657e7eecf949415b0f3132e08
Oliver Blanthorn <freedom4cows@gmail.com>
2023-06-11 17:56:42 +0200

    REVERT ME: make debugging quicker

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -240,7 +240,7 @@ export class default_config {
         p: "clipboard open",
         P: "clipboard tabopen",
         j: "scrollline 10",
-        "<C-e>": "scrollline 10",
+        "<D-e>": "scrollline 10",
         k: "scrollline -10",
         "<C-y>": "scrollline -10",
         h: "scrollpx -50",
@@ -954,7 +954,7 @@ export class default_config {
     logging: { [key: string]: LoggingLevel } = {
         cmdline: "warning",
         containers: "warning",
-        controller: "warning",
+        controller: "debug",
         excmd: "error",
         hinting: "warning",
         messaging: "warning",
