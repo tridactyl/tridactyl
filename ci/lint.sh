2990bd053813a13d736a13a42f6eb029e9dc9575
Chris Holland <chrishollandaise@gmail.com>
2024-01-20 14:35:10 -0800

    add sed -i lint check to ensure portability

--- ci/lint.sh
+++ ci/lint.sh
@@ -5,5 +5,10 @@ if [ -x "$(command -v shellcheck)" ]; then
 else
 	echo "Warning: shellcheck is not installed, skipping shell scripts"
 fi
+incompatible_sed=$(! grep -RPn --color=always --exclude-dir=node_modules --exclude-dir=.git 'sed(?:\s-\w+)*\s-i(?!\S|\s"")')
+if [ "$incompatible_sed" ]; then
+	printf "\nWarning: avoid non-portable sed flag -i without backup extension, use -i.bak\n"
+	printf "%s\n\n" "$incompatible_sed"
+fi
 yarn run lint
 "$(yarn bin)/eslint" --rulesdir custom-eslint-rules --ext .ts .
--- scripts/changelog_mangler.sh
+++ scripts/changelog_mangler.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Replace issue numbers in brackets eg (#1337) with a link to the issue on the repository
-sed -i 's; (#\([0-9]*\)); ([#\1](https://github.com/tridactyl/tridactyl/issues/\1));g' CHANGELOG.md
+sed -i.bak 's; (#\([0-9]*\)); ([#\1](https://github.com/tridactyl/tridactyl/issues/\1));g' CHANGELOG.md
