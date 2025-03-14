b4be583cfc3058073c67433d620728011eaa0ef0
Colin Caine <cmcaine@gmail.com>
2025-03-14 14:31:42 +0000

    Explain what the config.test.ts test does

--- src/lib/config.test.ts
+++ src/lib/config.test.ts
@@ -7,7 +7,10 @@ const config = new default_config()
 // todo: test subconfigs and platform_defaults
 const nmaps = Object.keys(config.nmaps)
 
-for (let mode of Object.keys(config).filter(x=>x.match(/maps$/))) {
+// Test that all of the default maps use the canonical representation (otherwise they won't work correctly, because Tridactyl expects the maps in the config to be canonical; which now that I write it, that does seem like an obvious foot-gun for if we ever change the canonicalisation algorithm).
+//
+// But, hey, at least with this test we are more likely to notice if that happens and either not change the canonicalisation algorithm or introduce a migration.
+for (let mode of Object.keys(config).filter(x => x.match(/maps$/))) {
     const mapstrings = Object.keys(config[mode])
     testAll(canonicaliseMapstr, zip(mapstrings, mapstrings))
 }
