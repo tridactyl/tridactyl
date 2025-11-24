2c700182f8e2c2595286270a0e1a4a18f5970360
Oliver Blanthorn <freedom4cows@gmail.com>
2025-11-24 17:41:03 +0100

    Bake magic string to cheer up TS

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -71,7 +71,7 @@ export function bucketize(
 
 // thanks daniel metzner https://stackoverflow.com/questions/59412625/generate-random-uuid-javascript#tab-top
 export function uuidv4(): string {
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c: any) =>
         // eslint-disable-next-line no-bitwise
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     )
