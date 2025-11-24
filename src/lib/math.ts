d88fa4e97565a92ee8eadd2d459f2543c3259815
Oliver Blanthorn <freedom4cows@gmail.com>
2025-11-24 13:00:16 +0100

    Cheer up linter

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -70,11 +70,10 @@ export function bucketize(
 }
 
 // thanks daniel metzner https://stackoverflow.com/questions/59412625/generate-random-uuid-javascript#tab-top
-export function uuidv4(): string {  
-    // @ts-ignore  
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>  
-        // tslint:disable-next-line:no-bitwise  
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)  
+export function uuidv4(): string {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        // eslint-disable-next-line no-bitwise
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     )
 }
 
