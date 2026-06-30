09811efbe3b122c34df3f3757fbc34e496ea0383
Oliver Blanthorn <freedom4cows@gmail.com>
2026-06-30 13:22:51 +0200

    Fix default nmode binds

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -171,7 +171,7 @@ export class default_config {
         "<AC-Escape>": "mode normal",
         "<AC-`>": "mode normal",
         "<S-Escape>": "mode normal",
-        "<C-o>": "nmode normal 1 mode ignore",
+        "<CD-o><CU-o>": "nmode normal 1 mode ignore",
     }
 
     /**
@@ -263,7 +263,7 @@ export class default_config {
         "<C-d>": "scrollpage 0.5",
         "<C-f>": "scrollpage 1",
         "<C-b>": "scrollpage -1",
-        "<C-v>": "nmode ignore 1 mode normal", // Is this a terrible idea? Pentadactyl did it http://bug.5digits.org/help/pentadactyl/browsing.xhtml#send-key
+        "<CD-v><CU-v>": "nmode ignore 1 mode normal",
         $: "scrollto 100 x",
         // "0": "scrollto 0 x", // will get interpreted as a count
         "^": "scrollto 0 x",
