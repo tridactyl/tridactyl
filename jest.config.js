894f4d78a614e4198e8c603c005750bdf33d59ed
Oliver Blanthorn <freedom4cows@gmail.com>
2026-07-19 13:12:08 +0200

    Update to jest 27

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ const tsConfig = require('./tsconfig');
 
 module.exports = {
   preset: "ts-jest",
+  testEnvironment: "jsdom",
+  testRunner: "jest-jasmine2",
   setupFiles: [
     "jest-webextension-mock"
   ],
@@ -11,7 +13,7 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   globals: {
     "ts-jest": {
-      tsConfig: {
+      tsconfig: {
         ...tsConfig.compilerOptions,
           types: ["jest", "node", "@types/firefox-webext-browser"]
       },
