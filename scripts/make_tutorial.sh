2bf0e5bd54e15f9ab266218331e3b93557312bcd
Oliver Blanthorn <freedom4cows@gmail.com>
2026-07-23 17:32:15 +0200

    Fix #5311: add nav pane to :tutor

--- scripts/make_tutorial.sh
+++ scripts/make_tutorial.sh
@@ -13,7 +13,8 @@ dest="../../../generated/static/clippy/"
 for page in $pages
 do
     fileroot=$(echo "$page" | cut -d'.' -f-2)
-    sed "/REPLACETHIS/,$ d" tutor.template.html > "$dest$fileroot.html"
+    sed -e "s|href=\"$fileroot.html\"|aria-current=\"page\" &|" \
+        -e "/REPLACETHIS/,$ d" tutor.template.html > "$dest$fileroot.html"
     "$(yarn bin)/marked" "$page" >> "$dest$fileroot.html"
     sed "1,/REPLACETHIS/ d" tutor.template.html >> "$dest$fileroot.html"
     sed -i.bak "s|\(href=['\"]\./[^'\"]*\)\.md\(['\"]\)|\1.html\2|g" "$dest$fileroot.html"
